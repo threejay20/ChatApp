c95c558bf93f3ef2e9e68a6baec7e62ec015a2b4
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-07-03 19:34:35 -0400

    forms

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -5,6 +5,7 @@ import {
     MultiChatWindow } 
     from 'react-chat-engine-advanced';
 import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 
 const Chat = () => {
     const chatProps = useMultiChatLogic(
@@ -19,6 +20,11 @@ const Chat = () => {
         {...chatProps}
         style={{ height: "100vh"}}
         renderChatHeader={(chat) => <Header chat={chat}/>}
+        renderMessageForm={(props) => {
+            return (
+                <StandardMessageForm props={props} activeChat={chatProps.chat} />
+            )
+        }}
     
      />
     </div>
