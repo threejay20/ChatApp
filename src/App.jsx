269d52474f187960f80448d912f2bdd9b56c313b
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-07-03 19:15:33 -0400

    dashboard starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Chat from "./components/chat";
+import Chat from "@/components/chat";
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
   
   </div>
   );
-  
+
         
         }
        
--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { 
+    useMultiChatLogic, 
+    MultiChatSocket, 
+    MultiChatWindow } 
+    from 'react-chat-engine-advanced';
+import Header from "@/components/customHeader";
 
 const Chat = () => {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID, 
+        "testuser",
+        "1234"
+        )
   return (
-    <div>Chat</div>
+    <div style={{flexBasis: "100%"}}>
+        <MultiChatSocket {...chatProps}/>
+        <MultiChatWindow 
+        {...chatProps}
+        style={{ height: "100vh"}}
+        renderChatHeader={(chat) => <Header chat={chat}/>}
+    
+     />
+    </div>
   )
-}
+};
 
 export default Chat;
